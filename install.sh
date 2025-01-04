ca10f3f0c3b24826433c13955260eb111635a15f
SoloDactyl <flayerhost@gmail.com>
2025-01-04 09:25:52 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,24 +9,16 @@ read -p "Enter your choice: " choice
 
 if [[ $choice -eq 1 ]]; then
 
-    echo "Setting up SoloDactyl Panel..."
-    # Install Node.js and dependencies
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
     sudo apt update
     sudo apt install -y nodejs git
-
-    # Clone SoloDactyl repository
     git clone https://github.com/Solodactyl/solodactyl-panel.git
     cd solodactyl-panel
-
-    # Install dependencies and set up the panel
     npm install
     npm run seed
     npm run createUser
-
-    # Run the panel
     node .
 
     echo "Installation completed! SoloDactyl Panel is running."
